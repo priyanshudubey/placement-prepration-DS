c33de670589f4bb0cfacd57cb8350e533a24cdac
priyanshu <priyanshu0dubey@gmail.com>
2021-01-19 20:13:01 +0530

    Basics

---
+++ power.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool isPrime(int n)
+{
+    bool isPrime = true;
+    for(int i=2;i<=int(sqrt(n));i++){
+        if(n%i==0){
+            isPrime = false;
+            break;
+        }
+    }
+    if(isPrime && n!=1)
+    return true;
+    else
+    return false;
+}
+int exactly3Divisors(int N)
+{
+    int count = 0;
+    for(int i=2;i<=sqrt(N);i++){
+        if(isPrime(int(i))){
+            count++;
+        }
+    }
+    return count;
+}
+int main(){
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("op.txt","w",stdout);
+	#endif
+	int a;
+	cin>>a;
+	cout<<exactly3Divisors(a);
+	return 0;
+}
